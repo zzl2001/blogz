d90dbdfe55c4abb6f8df8cdaf2c5d9a48c753c43
zzl <33721180@qq.com>
2021-06-09 22:57:26 +0800

    2-2 init

---
+++ src/main/java/com/zzl/blogz/BlogzApplication.java
@@ -0,0 +1,13 @@
+package com.zzl.blogz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzl/blogz/BlogzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzl.blogz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
