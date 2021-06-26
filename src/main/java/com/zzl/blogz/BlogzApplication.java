35448e3c755de18d7fbd54759b4ad63cfc4630d1
zzl <33721180@qq.com>
2021-06-26 21:50:10 +0800

    2-5 修改启动类并添加Log打印信息

--- src/main/java/com/zzl/blogz/BlogzApplication.java
+++ src/main/java/com/zzl/blogz/BlogzApplication.java
@@ -1,13 +1,21 @@
 package com.zzl.blogz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BlogzApplication {
 
+    private static Logger LOG = LoggerFactory.getLogger(BlogzApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(BlogzApplication.class, args);
+        SpringApplication app = new SpringApplication(BlogzApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功!");
+        LOG.info("地址:\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 
 }
