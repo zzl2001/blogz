196613c0bab640dbaf305ab8475048b617127b1a
zzl <33721180@qq.com>
2021-06-27 21:41:02 +0800

    2-9 集成热部署

---
+++ doc/all.sql
@@ -0,0 +1,11 @@
+select * from other;
+drop table if exists `other`;
+
+drop table if exists `test`;
+create table `test` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4
+  comment='名称';
