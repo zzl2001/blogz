40b41d59158be5f13b3f1eb4fa36b36c2a37fee4
zzl <33721180@qq.com>
2021-06-28 19:28:52 +0800

    3-6 增加通用返回类

---
+++ src/main/java/com/zzl/blogz/controller/EbookController.java
@@ -0,0 +1,32 @@
+package com.zzl.blogz.controller;
+
+import com.zzl.blogz.domain.Ebook;
+import com.zzl.blogz.resp.CommonResp;
+import com.zzl.blogz.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author Zz1
+ * @version 1.0
+ * @date 2021/6/26 22:17
+ */
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public CommonResp list(){
+        CommonResp<List<Ebook>> response = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        response.setContent(list);
+        return response;
+    }
+}
