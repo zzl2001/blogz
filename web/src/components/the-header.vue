fe83b6b27e9b59874bbda074ec46c18cea32f037
zzl <33721180@qq.com>
2021-07-01 15:16:50 +0800

    4-7 header的制作

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,26 @@
+<template>
+    <a-layout-header class="header">
+        <div class="logo" />
+        <a-menu
+                theme="dark"
+                mode="horizontal"
+                v-model:selectedKeys="selectedKeys1"
+                :style="{ lineHeight: '64px' }"
+        >
+            <a-menu-item key="1">nav 1</a-menu-item>
+            <a-menu-item key="2">nav 2</a-menu-item>
+            <a-menu-item key="3">nav 3</a-menu-item>
+        </a-menu>
+    </a-layout-header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-header',
+        // props: { //props用于父子组件之间传递数据，参考helloworld写法
+        //     msg: String,
+        // },
+    });
+</script>
