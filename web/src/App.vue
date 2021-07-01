48ada7b0f5a63bd893e5393cd7a641478a1a46e3
zzl <33721180@qq.com>
2021-07-01 15:26:27 +0800

    4-7 header footer的制作

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <the-header/>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Ant Design ©2018 Created by Ant UED
-    </a-layout-footer>
+    <the-footer/>
   </a-layout>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import TheHeader from '@/components/the-header.vue';
+  import TheFooter from '@/components/the-footer.vue';
 
   export default defineComponent({
     name: 'app',
     components: {
-      TheHeader
+      TheHeader,
+      TheFooter
     }
   })
 </script>
